b25cc3e30edb8d88afd3a358b641a918d0c529f2
MohaMedFRy <mohamedfry026@gmail.com>
2020-07-28 01:43:15 +0200

    User&Owner

--- app/src/main/java/com/example/pharmacyapp/MainActivity.java
+++ app/src/main/java/com/example/pharmacyapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.pharmacyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button own,use;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        own =findViewById(R.id.owner);
+        use =findViewById(R.id.user);
+        own.setOnClickListener(this);
+        use.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.user){
+            Toast.makeText(MainActivity.this,"User",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(MainActivity.this,Login.class));
+        }else if (view.getId()==R.id.owner){
+            Toast.makeText(MainActivity.this,"Owner",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(MainActivity.this,Login.class));
+        }
     }
 }
