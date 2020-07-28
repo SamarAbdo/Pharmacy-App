879bd9ec58488565b76f64c5e12e5d1d0d6091ae
MohaMedFRy <mohamedfry026@gmail.com>
2020-07-28 19:19:44 +0200

    Validation

--- app/src/main/java/com/example/pharmacyapp/SignUp.java
+++ app/src/main/java/com/example/pharmacyapp/SignUp.java
@@ -9,13 +9,14 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
-    EditText username,password,Conpassword,email,address;
+    EditText firstName,lastName,password,Conpassword,email,address;
     Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        username=findViewById(R.id.userSign);
+        firstName=findViewById(R.id.fname);
+        lastName=findViewById(R.id.lname);
         password=findViewById(R.id.pass);
         Conpassword=findViewById(R.id.passCon);
         address=findViewById(R.id.addressSign);
@@ -24,10 +25,32 @@ public class SignUp extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                if(!nameOk(firstName.getText().toString()))firstName.setError("First Name not Valid");
+                if(!nameOk(lastName.getText().toString()))lastName.setError("Last Name not Valid");
+                if (email.getText().toString().isEmpty())email.setError("Not Valid");
+                if (address.getText().toString().isEmpty())address.setError("Not Valid");
+                if (!passOk(password.getText().toString()))password.setError("Not Valid");
+                if (password.getText().toString()!=Conpassword.getText().toString())Conpassword.setError("Not the same password");
+
             }
         });
 
+    }
+    public boolean nameOk(String s){
+        if (s.isEmpty())return false;
+        for (int i=0;i<s.length();i++)
+            if (s.charAt(i)<='A'||s.charAt(i)>='z')return false;
+        return true;
+    }
+    public boolean passOk(String p){
+        if (p.length()<8)return false;
+        int f=0,l=0;
+        for (int i=0;i<p.length();i++){
+            if(p.charAt(i)>='A'&&p.charAt(i)<='z')f=1;
+            if (p.charAt(i)>='0'&&p.charAt(i)<='9')l=1;
+        }
+        if (f==1&&l==1)return true;
+        return false;
     }
 
     public void onClktxt(View view) {
