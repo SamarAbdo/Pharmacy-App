3d8391ce72c8e43b37e89223b37c0aadf5a8280e
samar <samerabdo392@gmail.com>
2020-07-28 15:55:50 +0200

    signup txt click and adding signup acc

--- app/src/main/java/com/example/pharmacyapp/Login.java
+++ app/src/main/java/com/example/pharmacyapp/Login.java
@@ -2,13 +2,27 @@ package com.example.pharmacyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+Button LoginBtn;
+TextView SignUpTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LoginBtn=findViewById(R.id.loginbtn);
+        SignUpTxt=findViewById(R.id.signuptxt);
+
+    }
+
+    public void onClicktxt(View view) {
+        Intent intent=new Intent(this,SignUp.class);
+        startActivity(intent);
+
     }
 }
