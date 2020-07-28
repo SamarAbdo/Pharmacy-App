0c7d5c35a85cea5828305a18bbbd65abfead6650
samar <samerabdo392@gmail.com>
2020-07-29 01:50:25 +0200

    login background

---
+++ app/src/main/java/com/example/pharmacyapp/model_class.java
@@ -0,0 +1,12 @@
+package com.example.pharmacyapp;
+
+public class model_class {
+    String PharmacyName,PharmacyAddress,PharmacyNum,PharmacyImg;
+
+    public model_class(String pharmacyName, String pharmacyAddress, String pharmacyNum, String pharmacyImg) {
+        PharmacyName = pharmacyName;
+        PharmacyAddress = pharmacyAddress;
+        PharmacyNum = pharmacyNum;
+        PharmacyImg = pharmacyImg;
+    }
+}
