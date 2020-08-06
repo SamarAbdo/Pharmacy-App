972a1837fe921093eb5ec07ca9636face2f468b6
MohaMedFRy <mohamedfry026@gmail.com>
2020-08-06 20:58:29 +0200

    Upload Image

--- app/src/main/java/com/example/pharmacyapp/AddPharmacy.java
+++ app/src/main/java/com/example/pharmacyapp/AddPharmacy.java
@@ -1,15 +1,23 @@
 package com.example.pharmacyapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddPharmacy extends AppCompatActivity {
     EditText namePharmacy,numPharmacy,addressPharmacy,facbookAcc;
-    Button addbtn;
+    Button addbtn,uploadbtn;
+    ImageView userImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +35,21 @@ public class AddPharmacy extends AppCompatActivity {
                 if(addressPharmacy.getText().toString().isEmpty())addressPharmacy.setError("Not Valid");
             }
         });
+        uploadbtn=findViewById(R.id.uploadImage);
+        userImg=findViewById(R.id.userImage);
+        uploadbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(in,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap= (Bitmap) data.getExtras().get("data");
+        userImg.setImageBitmap(bitmap);
     }
 }
