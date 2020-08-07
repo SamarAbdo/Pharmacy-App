45e6e23139fd6294e046d805adea0fd863fe8960
MohaMedFRy <mohamedfry026@gmail.com>
2020-08-07 02:52:12 +0200

    mail validation

--- app/src/main/java/com/example/pharmacyapp/SignUp.java
+++ app/src/main/java/com/example/pharmacyapp/SignUp.java
@@ -27,7 +27,7 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View view) {
                 if(!nameOk(firstName.getText().toString()))firstName.setError("First Name not Valid");
                 if(!nameOk(lastName.getText().toString()))lastName.setError("Last Name not Valid");
-                if (email.getText().toString().isEmpty())email.setError("Not Valid");
+                if (!mailOk(email.getText().toString()))email.setError("Not Valid");
                 if (address.getText().toString().isEmpty())address.setError("Not Valid");
                 if (!passOk(password.getText().toString()))password.setError("Not Valid");
                 if (password.getText().toString()!=Conpassword.getText().toString())Conpassword.setError("Not the same password");
@@ -50,6 +50,20 @@ public class SignUp extends AppCompatActivity {
             if (p.charAt(i)>='0'&&p.charAt(i)<='9')l=1;
         }
         if (f==1&&l==1)return true;
+        return false;
+    }
+    public boolean mailOk(String m){
+        if(m.isEmpty())return false;
+        int a=0,b=0,c=0,d=0,e=0;
+        for (int i=0;i<m.length();i++){
+            if (m.charAt(i)>='A'&&m.charAt(i)<='z'&&b==0)a=1;
+            else if (m.charAt(i)=='@'&&a==1&&b==0)b=1;
+            else if (m.charAt(i)>='A'&&m.charAt(i)<='z'&&b==1&&d==0)c=1;
+            else if (m.charAt(i)=='.'&&c==1&&d==0)d=1;
+            else if (m.charAt(i)>='A'&&m.charAt(i)<='z'&&d==1)e=1;
+            else return false;
+        }
+        if (e==1)return true;
         return false;
     }
 
