84d5727a0cd8be678fceb98fc192060b64247179
MohaMedFRy <mohamedfry026@gmail.com>
2020-08-07 04:43:53 +0200

    upload Images lsa m4 rady yshtaghal ^_^

--- app/src/main/java/com/example/pharmacyapp/AddPharmacy.java
+++ app/src/main/java/com/example/pharmacyapp/AddPharmacy.java
@@ -1,11 +1,15 @@
 package com.example.pharmacyapp;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
@@ -18,6 +22,7 @@ public class AddPharmacy extends AppCompatActivity {
     EditText namePharmacy,numPharmacy,addressPharmacy,facbookAcc;
     Button addbtn,uploadbtn;
     ImageView userImg;
+    int REQUEST_CAMERA=1,SELECT_FILE=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +45,28 @@ public class AddPharmacy extends AppCompatActivity {
         uploadbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(in,0);
+                final CharSequence[] items={"Camera","Gallery","Cancel"};
+                AlertDialog.Builder builder=new AlertDialog.Builder(AddPharmacy.this);
+                builder.setTitle("Add Image");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(items[i].equals("Camera")){
+                            Intent in=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(in,REQUEST_CAMERA);
+                        }else if (items[i].equals("Gallery")){
+                            Intent in=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            in.setType("image/*");
+                            startActivityForResult(in.createChooser(in,"Select File"),SELECT_FILE);
+                        }else if (items[i].equals("Cancel")){
+                            dialogInterface.dismiss();
+                        }
+                    }
+
+                });builder.show();
+
+
+
             }
         });
     }
@@ -49,7 +74,15 @@ public class AddPharmacy extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap= (Bitmap) data.getExtras().get("data");
-        userImg.setImageBitmap(bitmap);
+        if (requestCode== Activity.RESULT_OK){
+            if (requestCode==REQUEST_CAMERA){
+                Bitmap bitmap= (Bitmap) data.getExtras().get("data");
+                userImg.setImageBitmap(bitmap);
+            }else if (requestCode==SELECT_FILE){
+                Uri selectImg=data.getData();
+                userImg.setImageURI(selectImg);
+            }
+        }
+
     }
 }
